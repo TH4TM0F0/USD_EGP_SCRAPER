e6a99442a6dff0ed6ceee2ee0125988f7081fec4
TH4TM0F0 <https://github.com/TH4TM0F0>
2026-03-01 10:36:17 +0200

    [FEAT]: Scraping module

---
+++ src/scraper.py
@@ -0,0 +1,67 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+import csv
+import os
+from datetime import datetime, timezone, timedelta
+
+DATA_DIR = 'data'
+CSV_DIR = 'exchange_rate.csv'
+UTILS_DIR = 'utils'
+TXT_DIR = 'metadata.txt'
+headers = ['TIMESTAMP', 'RATE (EGP)']
+
+tz = timezone(timedelta(hours=2))
+
+url = "https://wise.com/us/currency-converter/usd-to-egp-rate"
+
+def save_to_csv(data : dict) -> bool:
+    os.makedirs(DATA_DIR , exist_ok = True)
+    data_path = os.path.join(DATA_DIR , CSV_DIR)
+    file_exists_flag = os.path.exists(data_path)
+    with open(file = data_path , mode = 'a') as f:
+        if not file_exists_flag:
+            f.write(','.join(headers))
+            f.write('\n')
+
+        f.write(str(data['timestamp']))
+        f.write(',')
+        f.write(data['rate'])
+        f.write('\n')
+
+def main():
+    try:
+        response = requests.get(url , timeout = 10)
+        response.raise_for_status()
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        for tag in soup.find_all('span', dir="ltr"):
+            exchange_price = tag.text.split('=')
+
+            if len(exchange_price) < 2:   
+                continue
+
+            if 'EGP' in exchange_price[0]:
+                rate = exchange_price[0].split(' ')[0]
+            else:
+                rate = exchange_price[1].split(' ')[1]
+
+            try:                          
+                float(rate)
+            except ValueError:
+                continue
+
+            print(rate)
+            data_frame = {
+                'timestamp' : datetime.now(tz).strftime('%Y-%m-%d %H:%M:%S %Z') ,
+                'rate' : rate
+                }
+            save_to_csv(data_frame)
+            break
+
+    except requests.RequestException as error:
+        print(f'Error: {error}')
+
+if __name__ == '__main__':
+    main()
